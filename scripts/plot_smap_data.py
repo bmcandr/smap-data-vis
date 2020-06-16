30bbdde2323ff6389f6ea6af5445030a3f89d218
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2020-06-16 11:53:54 -0400

    rename script

--- scripts/plot_smap_data.py
+++ scripts/plot_smap_data.py
@@ -1,46 +1,80 @@
-"""
+'''
 This script reads SMAP data and plots it.
-"""
+'''
 
+# import libraries/modules needed:
+
+# the os module is used to create paths to data
 import os
 
+# the Path module is used to get paths to files of interest
 from pathlib import Path as path
+
+# matplotlib is used to plot data
+# (anim is a submodule that can be used to create animations)
 from matplotlib import pyplot as plt
 # from matplotlib import animation as anim
+
+# Basemap is a module used for plotting maps
 from mpl_toolkits.basemap import Basemap
 
+# rasterio is a module used to read/write geospatial raster data
 import rasterio as rio
+
+# rasterio reads raster data into numpy arrays so we need numpy too
 import numpy as np
 
-DATA_DIR = os.path.join('data', 'testcase5_ldt_obscdf')
-RS_DATA_DIR = os.path.join(DATA_DIR, 'RS_DATA')
-SMAP_DATA_DIR = os.path.join(RS_DATA_DIR, 'SMAP', 'SPL3SMP.005')
-SM_FILE_SUFFIX = '*_R16010_001.h5'
+# creating paths using the os.path.join method enables
+# cross-platform compatability
+DATA_DIR = os.path.join('..', 'data')
+INPUT_DATA_DIR = os.path.join(DATA_DIR, 'input')
+SMAP_DATA_DIR = os.path.join(INPUT_DATA_DIR, 'SMAP', 'SPL3SMP.005')
 
-EASE_GRID_DIR = os.path.join('data', 'ease_grid')
+# file suffix pattern for the SMAP files of interest
+SMAP_FILE_SUFFIX = '*_R16010_001.h5'
+
+# get paths to all HDF5 files ending with SMAP_FILE_SUFFIX
+# found in all directories within SMAP_DATA_DIR
+DATA_PATHS = sorted([str(filepath) for filepath in path(SMAP_DATA_DIR).rglob(SMAP_FILE_SUFFIX)])
+
+# get the first in path DATA_PATHS to use for testing
+TEST_DATA_PATH = DATA_PATHS[0]
+
+# the HDF5 files contain many datasets, but
+# the soil_moisture dataset is what we're interested in
+SM_DATASET_NAME = 'soil_moisture'
+
+# create a path to an output directory in case
+# we want to save files at some point
+OUTPUT_DIR = os.path.join(DATA_DIR, 'output')
+
+# check if OUTPUT_DIR exists and, if not, create it
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+# create variables to hold path to files containing EASE grid lat/lons (used for plotting)
+# data source: https://github.com/TUW-GEO/ease_grid/tree/master/tests/test_data
+EASE_GRID_DIR = os.path.join(DATA_DIR, 'ease_grid')
 EASE_LATS_PATH = os.path.join(EASE_GRID_DIR, 'EASE2_M36KM.lats.964x406x1.double')
 EASE_LONS_PATH = os.path.join(EASE_GRID_DIR, 'EASE2_M36KM.lons.964x406x1.double')
 
-PLOT_OUT_DIR = os.path.join('data', 'output', 'sm_anim_frames')
+# read EASE2 lat/lons and reshape from a 1D array into a 2D array matching the EASE Grid shape
+EASE_SHAPE = (406, 964)
+EASE_LATS = np.fromfile(EASE_LATS_PATH, dtype=np.float64).reshape(EASE_SHAPE)
+EASE_LONS = np.fromfile(EASE_LONS_PATH, dtype=np.float64).reshape(EASE_SHAPE)
 
-SM_DATASET_NAME = 'soil_moisture'
-
-TEST_DATA_PATH = os.path.join(SMAP_DATA_DIR, '2017.03.01', SM_FILE_SUFFIX)
-
-# get paths to all h5 files
-DATA_PATHS = sorted([str(fp) for fp in path(SMAP_DATA_DIR).rglob(SM_FILE_SUFFIX)])
-
+# store the EPSG projection code for the EASE Grid
+# that this data is projected in
+# this info is useful if we want to reproject the data at some point
 EASE_EPSG = 'epsg:3410'
-WGS84_EPSG = 'epsg:3426'
 
-# read EASE2 lat/lons
-EASE_LATS = np.fromfile(EASE_LATS_PATH, dtype=np.float64).reshape((406, 964))
-EASE_LONS = np.fromfile(EASE_LONS_PATH, dtype=np.float64).reshape((406, 964))
+# set output figure size
+FIG_SIZE = (30, 10)
 
 def print_dataset_names(src_path: str):
-    """
+    '''
     Print name of datasets in file at src_path.
-    """
+    '''
 
     with rio.open(src_path) as src:
         subdatasets = src.subdatasets
@@ -49,9 +83,9 @@ def print_dataset_names(src_path: str):
 
 
 def read_dataset(src_path: str, dataset_name: str, mask_nodata: bool = True):
-    """
+    '''
     Read dataset_name in HDF5 file at src_path
-    """
+    '''
 
     with rio.open(src_path) as src:
         subdatasets = src.subdatasets
@@ -71,9 +105,9 @@ def read_dataset(src_path: str, dataset_name: str, mask_nodata: bool = True):
 
 
 def read_coords(src_path: str):
-    """
+    '''
     Read and return lat/lon from HDF5 file at src_path.
-    """
+    '''
 
     with rio.open(src_path) as src:
         subdatasets = src.subdatasets
@@ -91,9 +125,9 @@ def read_coords(src_path: str):
 
 
 def save_plots(data_paths: list, dataset_name: str):
-    """
+    '''
     Save plots of data.
-    """
+    '''
 
     for data_path in data_paths:
         plot_date = data_path.split('/')[-2]
@@ -101,8 +135,23 @@ def save_plots(data_paths: list, dataset_name: str):
         plot_data(data, dataset_name, desc)
         plt.title(plot_date)
 
-        out_fp = os.path.join(PLOT_OUT_DIR, f'{plot_date}.png')
+        out_fp = os.path.join(OUTPUT_DIR, f'{plot_date}.png')
         plt.savefig(out_fp)
+
+
+def calc_mean_dataset(data_paths: list, dataset_name: str):
+    '''
+    Read in data and calculate mean.
+    '''
+
+    datasets = []
+    meta = None
+    desc = None
+    for data_path in data_paths:
+        dataset, meta, desc = read_dataset(data_path, dataset_name)
+        datasets.append(dataset)
+
+    return np.dstack(datasets), meta, desc
 
 
 def plot_data(data: np.ndarray, \
@@ -110,9 +159,9 @@ def plot_data(data: np.ndarray, \
                     lats: np.ndarray = EASE_LATS, lons: np.ndarray = EASE_LONS, \
                     projection: str = 'cyl', cmap: str = 'viridis_r'):
 
-    """
+    '''
     Plot data.
-    """
+    '''
 
     long_name, units, vmin, vmax = (None, None, None, None)
 
@@ -126,7 +175,7 @@ def plot_data(data: np.ndarray, \
         elif 'valid_max' in descriptor:
             vmax = dataset_desc[descriptor]
 
-    fig, ax = plt.subplots(figsize=(30, 10))
+    fig, ax = plt.subplots(figsize=FIG_SIZE)
 
     if projection == 'robin':
         map_plot = Basemap(projection='robin', resolution='c', lat_0=0, lon_0=0)
@@ -151,30 +200,15 @@ def plot_data(data: np.ndarray, \
     map_plot.drawmeridians(meridians, color='gray', labels=labels)
     map_plot.drawlsmask(ocean_color='lightblue')
 
-    # plt.tight_layout()
     plt.show()
-
-def gen_mean_dataset(data_paths: list, dataset_name: str):
-    """
-    Read in data and calculate mean.
-    """
-
-    datasets = []
-    meta = None
-    desc = None
-    for data_path in data_paths:
-        dataset, meta, desc = read_dataset(data_path, dataset_name)
-        datasets.append(dataset)
-
-    return np.dstack(datasets), meta, desc
 
 
 def test_plot_mean():
-    """
+    '''
     Test plotting mean of all soil moisture datasets
-    """
+    '''
 
-    test_dataset, test_meta, test_desc = gen_mean_dataset(DATA_PATHS, SM_DATASET_NAME)
+    test_dataset, test_meta, test_desc = calc_mean_dataset(DATA_PATHS, SM_DATASET_NAME)
     mean_sm = np.nanmean(test_dataset, axis=2)
     plot_data(mean_sm, SM_DATASET_NAME, test_desc)
 
